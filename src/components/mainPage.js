e8e93b67604aac43f2ddda59f272709b942a072f
Jordan Duncan <jordandunc@gmail.com>
2020-02-05 01:09:41 -0800

    add search input that filters

--- src/components/mainPage.js
+++ src/components/mainPage.js
@@ -4,23 +4,39 @@ import MainPageAvatar from "./mainPageAvatar";
 class MainPage extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      pokemonSearch: ""
+    };
   }
 
   componentDidMount() {}
 
   render() {
+    var patt = new RegExp(this.state.pokemonSearch);
     return (
       <div>
+        <div className="text-input-container">
+          <input
+            style={{ width: "50%", height: 30 }}
+            placeholder={"Search for a Pokemon"}
+            val={this.state.pokemonSearch}
+            onChange={e => {
+              this.setState({ pokemonSearch: e.target.value });
+            }}
+          />
+        </div>
         <div className="main-page-pokemon">
           {this.props.pokemon.map((pokemon, index) => {
-            return (
-              <MainPageAvatar
-                id={index}
-                image={pokemon.info ? pokemon.info.sprites.front_default : ""}
-                name={pokemon.name}
-              />
-            );
+            if (!this.state.pokemonSearch.length || patt.test(pokemon.name)) {
+              return (
+                <MainPageAvatar
+                  id={index}
+                  image={pokemon.info ? pokemon.info.sprites.front_default : ""}
+                  name={pokemon.name}
+                />
+              );
+            }
+            return "";
           })}
         </div>
       </div>
